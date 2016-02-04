4bf825e79dc1565079d90513290f17fd8cedf31b
bananasplitter <tiana_pehle@yahoo.com>
2016-02-04 13:38:26 -0800

    copied from mikes repo

---
+++ 15-Kanban-API/15-Kanban-API/Models/ListModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _15_Kanban_API.Models
+{
+    public class ListModel
+    {
+        
+        
+        public int ListId { get; set; }
+        public string Name { get; set; }
+        public System.DateTime CreatedDate { get; set; }
+        public string ListUrl
+        {
+            get
+            {
+                return $"http://localhost:50047/api/lists/{ListId}/cards";
+            }
+        }
+    }
+}
